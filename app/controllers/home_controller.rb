16f72204dc38f658926d9f0f42fa9b1154118487
Cesar Diaz <cesar@mediyak.com>
2016-03-02 17:05:20 -0300

    added kaminari gem for pagination, added cities action and route, restructured html files

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,8 @@
 class HomeController < ApplicationController
   def index
   end
+
+  def cities
+    @cities = Country.find_by(code: params['country'] ||= 'AR').cities.order(:name).page(params[:page] ||= 1).per(5)
+  end
 end
